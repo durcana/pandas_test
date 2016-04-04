be9919b0f6cbb1346a5d67029bec4f38c9025638
Amy Durcan <Amy@Amys-MacBook-Air.local>
2016-04-04 10:49:38 -0400

    Condense csv parser

--- csv_parser.py
+++ csv_parser.py
@@ -3,37 +3,31 @@ import os
 import pandas as pd
 
 
-# Adds the module column to specific data set.
-def add_column_themodule(file):
-    file['themodule'] = pd.Series([0] * (len(file.index)))
-    return file
-
-
-# Combine groups of data sets together through pandas. Reset indexing to read as one full data set.
-def combine_data(fileset):
-    df = pd.concat(f for f in fileset).reset_index()
-    return df
-
-
-# Group files into two groups, wish list and check out. Exclude ugids files
+# Group files into two groups, wish list and check out. Exclude ugids files.
 def group_files():
     path = '/Users/Amy/ucg_test/'
     wl_files = glob.glob(os.path.join(path, '*wl.csv'))
     co_files = glob.glob(os.path.join(path, '*co.csv'))
     return wl_files, co_files
 
+
 # Combine data from grouped files each into a single Pandas data frame.
-def combine_files(file_list):
+def combine_files(fileset):
+    # List for data after turned into Panda series.
     panda_data = []
-    for file in file_list:
+    for file in fileset:
         f = pd.read_csv(file)
+        # Adds the module column to specific data set if needed.
         if 'themodule' not in f.columns:
-            add_column_themodule(f)
+            f['themodule'] = pd.Series([0] * (len(f.index)))
         panda_data.append(f)
 
+    # Combine list of data series to single data frame. Reset indexing to read as one data set.
+    return pd.concat(f for f in panda_data).reset_index()
 
-    return combine_data(panda_data)
 
 wl, co = group_files()
+
+# Wish list and check out data each turned into single Pandas data frame.
 wl = combine_files(wl)
 co = combine_files(co)
