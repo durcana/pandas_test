beed529899b158760cacf908bca2ba160fb29828
Amy Durcan <Amy@Amys-MacBook-Air.local>
2016-04-05 10:43:37 -0400

    Script written on ipython notebook for analysis

--- csv_parser.py
+++ csv_parser.py
@@ -3,6 +3,15 @@ import os
 import pandas as pd
 
 
+# def get_test_info(file):
+#     # Read file as csv, sort is by _time.
+#     f = pd.read_csv(file).sort_values(by='_time', ascending=True)
+#     # Find first time and last time for test. This is to keep track when all the data is in one csv.
+#     first = f.at[1, '_time']
+#     last = f.at[len(f.index)-1, '_time']
+#     return first, last
+
+
 # Group files into two groups, wish list and check out. Exclude ugids files.
 def group_files():
     path = '/Users/Amy/ucg_test/'
@@ -19,10 +28,10 @@ def combine_files(fileset):
         f = pd.read_csv(file)
         # Adds the module column to specific data set if needed.
         if 'themodule' not in f.columns:
-            f['themodule'] = pd.Series([0] * (len(f.index)))
+            f['themodule'] = pd.Series(['0'] * (len(f.index)))
         panda_data.append(f)
 
-    # Combine list of data series to single data frame. Reset indexing to read as one data set.
+    # Combine list of data series to single data frame.
     return pd.concat(f for f in panda_data).reset_index()
 
 
@@ -31,3 +40,4 @@ wl, co = group_files()
 # Wish list and check out data each turned into single Pandas data frame.
 wl = combine_files(wl)
 co = combine_files(co)
+
